ec70317efddeb95848c10afab81a08e305a8a604
ubaid <ubaidilyas@gmail.com>
2020-06-23 23:13:20 +0200

    Tue, Jun 23, 2020 11:13:20 PM

---
+++ exercise_7/exercisenum7/out/clang-release/VoIPApp.o.d
@@ -0,0 +1,187 @@
+out/clang-release//VoIPApp.o: VoIPApp.cc \
+  D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/include\omnetpp.h \
+  D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/applications/udpapp/UdpBasicApp.h \
+  D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/common/INETDefs.h \
+  D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/common/precompiled_release.h \
+  D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/common/precompiled.h \
+  D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/opp_defines.h \
+  D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/features.h \
+  D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/common/Compat.h \
+  D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/common/InitStages.h \
+  D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/applications/base/ApplicationBase.h \
+  D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/common/lifecycle/ModuleOperations.h \
+  D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/common/lifecycle/LifecycleOperation.h \
+  D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/common/lifecycle/OperationalBase.h \
+  D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/common/lifecycle/ILifecycle.h \
+  D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/transportlayer/contract/udp/UdpSocket.h \
+  D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/common/packet/Message.h \
+  D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/common/packet/tag/TagSet.h \
+  D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/common/packet/Packet.h \
+  D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/common/packet/chunk/BitsChunk.h \
+  D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/common/packet/chunk/Chunk.h \
+  D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/common/MemoryInputStream.h \
+  D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/common/Units.h \
+  D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/common/INETMath.h \
+  D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/linklayer/common/MacAddress.h \
+  D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/networklayer/contract/ipv4/Ipv4Address.h \
+  D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/networklayer/contract/ipv6/Ipv6Address.h \
+  D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/common/MemoryOutputStream.h \
+  D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/common/Ptr.h \
+  D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/common/IntrusivePtr.h \
+  D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/common/packet/tag/RegionTagSet.h \
+  D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/common/TemporarySharedPtr.h \
+  D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/common/packet/chunk/BytesChunk.h \
+  D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/common/socket/ISocket.h \
+  D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/networklayer/common/L3Address.h \
+  D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/networklayer/common/ModuleIdAddress.h \
+  D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/networklayer/common/ModulePathAddress.h \
+  D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/networklayer/contract/clns/ClnsAddress.h \
+  D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/networklayer/contract/IInterfaceTable.h \
+  D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/networklayer/common/InterfaceEntry.h \
+  D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/common/Simsignals.h \
+  D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/common/Simsignals_m.h \
+  D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/common/INETDefs_m.h \
+  D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/networklayer/common/InterfaceToken.h \
+  D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/transportlayer/contract/udp/UdpControlInfo.h \
+  D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/transportlayer/contract/udp/UdpControlInfo_m.h \
+  D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/networklayer/common/IpProtocolId_m.h \
+  D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/networklayer/common/L3Address_m.h \
+  D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/applications/base/ApplicationPacket_m.h \
+  D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/common/packet/chunk/Chunk_m.h \
+  D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/common/Units_m.h \
+  D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/common/packet/chunk/BitCountChunk.h \
+  D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/common/packet/chunk/ByteCountChunk.h \
+  D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/common/packet/chunk/cPacketChunk.h \
+  D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/common/packet/chunk/SequenceChunk.h \
+  D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/common/packet/chunk/SliceChunk.h \
+  D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/common/packet/chunk/FieldsChunk.h \
+  D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/common/TimeTag_m.h \
+  D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/common/TagBase_m.h \
+  D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/networklayer/common/FragmentationTag_m.h \
+  D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/networklayer/common/L3AddressResolver.h \
+  D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/networklayer/contract/IRoutingTable.h \
+  D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/networklayer/contract/IRoute.h
+
+D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/include\omnetpp.h:
+
+D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/applications/udpapp/UdpBasicApp.h:
+
+D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/common/INETDefs.h:
+
+D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/common/precompiled_release.h:
+
+D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/common/precompiled.h:
+
+D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/opp_defines.h:
+
+D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/features.h:
+
+D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/common/Compat.h:
+
+D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/common/InitStages.h:
+
+D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/applications/base/ApplicationBase.h:
+
+D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/common/lifecycle/ModuleOperations.h:
+
+D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/common/lifecycle/LifecycleOperation.h:
+
+D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/common/lifecycle/OperationalBase.h:
+
+D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/common/lifecycle/ILifecycle.h:
+
+D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/transportlayer/contract/udp/UdpSocket.h:
+
+D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/common/packet/Message.h:
+
+D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/common/packet/tag/TagSet.h:
+
+D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/common/packet/Packet.h:
+
+D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/common/packet/chunk/BitsChunk.h:
+
+D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/common/packet/chunk/Chunk.h:
+
+D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/common/MemoryInputStream.h:
+
+D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/common/Units.h:
+
+D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/common/INETMath.h:
+
+D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/linklayer/common/MacAddress.h:
+
+D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/networklayer/contract/ipv4/Ipv4Address.h:
+
+D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/networklayer/contract/ipv6/Ipv6Address.h:
+
+D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/common/MemoryOutputStream.h:
+
+D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/common/Ptr.h:
+
+D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/common/IntrusivePtr.h:
+
+D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/common/packet/tag/RegionTagSet.h:
+
+D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/common/TemporarySharedPtr.h:
+
+D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/common/packet/chunk/BytesChunk.h:
+
+D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/common/socket/ISocket.h:
+
+D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/networklayer/common/L3Address.h:
+
+D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/networklayer/common/ModuleIdAddress.h:
+
+D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/networklayer/common/ModulePathAddress.h:
+
+D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/networklayer/contract/clns/ClnsAddress.h:
+
+D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/networklayer/contract/IInterfaceTable.h:
+
+D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/networklayer/common/InterfaceEntry.h:
+
+D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/common/Simsignals.h:
+
+D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/common/Simsignals_m.h:
+
+D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/common/INETDefs_m.h:
+
+D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/networklayer/common/InterfaceToken.h:
+
+D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/transportlayer/contract/udp/UdpControlInfo.h:
+
+D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/transportlayer/contract/udp/UdpControlInfo_m.h:
+
+D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/networklayer/common/IpProtocolId_m.h:
+
+D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/networklayer/common/L3Address_m.h:
+
+D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/applications/base/ApplicationPacket_m.h:
+
+D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/common/packet/chunk/Chunk_m.h:
+
+D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/common/Units_m.h:
+
+D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/common/packet/chunk/BitCountChunk.h:
+
+D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/common/packet/chunk/ByteCountChunk.h:
+
+D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/common/packet/chunk/cPacketChunk.h:
+
+D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/common/packet/chunk/SequenceChunk.h:
+
+D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/common/packet/chunk/SliceChunk.h:
+
+D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/common/packet/chunk/FieldsChunk.h:
+
+D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/common/TimeTag_m.h:
+
+D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/common/TagBase_m.h:
+
+D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/networklayer/common/FragmentationTag_m.h:
+
+D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/networklayer/common/L3AddressResolver.h:
+
+D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/networklayer/contract/IRoutingTable.h:
+
+D:/Parhai/Masters/2nd-Semester/Simulation_LL/omnetpp-5.6.1/inet-4.2.0-src/inet4/src\inet/networklayer/contract/IRoute.h:
